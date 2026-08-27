bcdba15c3a950a7182e5e5b1623e110c0fcdf379
ksaurabh252 <ksaurabh252@gmail.com>
2026-08-27 06:47:49 +0530

    feat: add group expenses route

--- server/routes/group.route.js
+++ server/routes/group.route.js
@@ -11,12 +11,16 @@ const {
 
 // Import authentication middleware
 const authMiddleware = require("../middleware/authMiddleware");
+const { getGroupExpenses } = require("../controllers/expenseController");
 
 // Create a new group - requires authentication
 router.post("/groups", authMiddleware, createGroup);
 
 // Add a member to a group
 router.post("/:groupId/members", authMiddleware, addMemberToGroup);
+
+// Get group expenses
+router.get("/:groupId/expenses", authMiddleware, getGroupExpenses);
 
 // Export router
 module.exports = router;
