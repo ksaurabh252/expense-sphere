a9fc45072c2e52122576087b1ec726f9dcc02bf9
ksaurabh252 <ksaurabh252@gmail.com>
2026-09-17 18:00:43 +0530

    build: configure path alias for src directory in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import path from "path";
+
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
+    react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
