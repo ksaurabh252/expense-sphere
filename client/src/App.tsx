98bf3644e0382929a590a7d644a2663bc998f1b0
ksaurabh252 <ksaurabh252@gmail.com>
2026-09-25 15:07:15 +0530

    feat: add groups, group details, and friends routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { Navigate, Route, Routes } from "react-router";
 import Auth from "./pages/Auth";
 import AppLayout from "./components/dashboard/AppLayout";
 import Dashboard from "./pages/Dashboard";
+import Friends from "./pages/Friends";
+import GroupDetails from "./pages/GroupDetails";
+import Groups from "./pages/Groups";
 import Notifications from "./pages/Notifications";
 import Placeholder from "./pages/Placeholder";
 
@@ -14,8 +17,9 @@ function App() {
       {/* Signed-in routes share one shell: sidebar + navbar + page content. */}
       <Route element={<AppLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/groups" element={<Placeholder title="Groups" />} />
-        <Route path="/friends" element={<Placeholder title="Friends" />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/groups/:groupId" element={<GroupDetails />} />
+        <Route path="/friends" element={<Friends />} />
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/profile" element={<Placeholder title="Profile" />} />
       </Route>
